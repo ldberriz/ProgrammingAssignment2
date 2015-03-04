45bb9ec4b17b2c39c119aa2e46c46b172ef797cb
ldberriz <ldberriz@kennis-research.com>
2015-03-03 22:29:30 -0500

    Minor changes to originals
    Correct typos and review code

--- cachematrix.R
+++ cachematrix.R
@@ -8,7 +8,7 @@ makeCacheMatrix <- function(x = matrix()) { # sets x equal to an empty matrix
   set <- function(y){  # Internal fx to makeCacheMatrix
     x <<- y     # set function assigns the argument to x.  Uses top x outside set()
     I <<- NULL  # re-set Matrix Inverse to NULL.  Points to the M object outside this fx
-  } ## so far noting has happened other than creating objects that live at the top of the heap
+  } ## so far nothing has happened other than creating objects that live at the top of the heap
   get <- function() x     # get function returns the matrix
   setInverse <- function(solve) I <<- solve
   # setInverse overrides the previous value of M and assigns the argument to 
@@ -25,12 +25,12 @@ makeCacheMatrix <- function(x = matrix()) { # sets x equal to an empty matrix
 ## and the matrix has not changed, it returns inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-  I<- x$getInverse()  # Retrives the most recent value for the inverse
+  I<- x$getInverse()  # Retrieves the most recent value for the inverse
   if(!is.null(I)){     # # If the value of Inverse exists (!is.null) gei it from the cache
     message("getting cached data")
     return(I)        
   }
-  # Otherwise retrive matrix x and calculate the inverse with the R solve() function
+  # Otherwise retrieve matrix x and calculate the inverse with the R solve() function
   message("newly calculating data")
   data <- x$get()
   I <- solve(data, ...)
